aa7961025e6ea0d3a09f252b2f48a3021b958498
Gabriel Murakami <egabrielmurakami@gmail.com>
2023-09-26 18:40:18 -0300

    dia 4

--- script.js
+++ script.js
@@ -1,29 +1,30 @@
-const loginButton = document.getElementById("login-form-submit");
-const form = document.getElementById("form");
+const btnLogin = document.getElementById("btnLogin");
+const cnpjForm = document.getElementById("cnpj");
 
-loginButton.addEventListener("click", (e) => {
-  e.preventDefault();
-  const cnpj = form.cnpj.value;
+btnLogin.addEventListener("click", function handleClick(event) {
+  event.preventDefault();
+  var cnpj = cnpjForm.value;
 
-  console.log(cnpj);
+  console.log("tamanho do texto " + cnpj.length);
 
-  switch (parseInt(cnpj)) {
-    case 1:
-      console.log("passou no 1");
-      alert("Bem vindo");
-      location.replace(
-        "https://bginformaticavinhedo.sharepoint.com/sites/SQQuimica/SitePages/ITHelpdeskHome.aspx"
-      );
-      /* window.open(
-        "https://bginformaticavinhedo.sharepoint.com/sites/SQQuimica/SitePages/ITHelpdeskHome.aspx"
-      ); */
-      break;
-    case 2:
-      console.log("passou no 2");
-      break;
-
-    default:
-      console.log("DEFAULT");
-      break;
+  if (cnpj.length < 5) {
+    alert("Digite os 5 números para continuar");
+    document.getElementById("cnpj").value = "";
+    return;
+  }
+  if (cnpj == 12345) {
+    console.log("SQ QUIMICA");
+    alert("Bem vindo! Voce será redirecionado ao seu painel de cliente.");
+    /* location.replace("https://bginformaticavinhedo.sharepoint.com/sites/SQQuimica/SitePages/ITHelpdeskHome.aspx") */
+    window.open(
+      "https://bginformaticavinhedo.sharepoint.com/sites/SQQuimica/SitePages/ITHelpdeskHome.aspx"
+    );
+    document.getElementById("cnpj").value = "";
+  }
+  if (cnpj == 09181) {
+    console.log("BG INFORMATICA");
+    alert("Bem vindo! Voce será redirecionado ao seu painel de cliente.");
+    window.open("https://bginformaticavinhedo.sharepoint.com/sites/ti");
+    document.getElementById("cnpj").value = "";
   }
 });
